583eea1a2dd7058ad00bab0cf126ae2e00e20cc9
iMath97 <camusmathias97@gmail.com>
2021-11-07 15:41:17 +0100

    ?commands added as command

--- controllers/commands.js
+++ controllers/commands.js
@@ -12,7 +12,7 @@ exports.initCommands = (client) => {
         break;
       case '?commands':
         command.reply({
-          content: "All commands start with '?'. \n \n List of basic commands: \n - ?joke: sends a funny joke \n - ?meme: sends a meme. \n \n List of musicplayer commands: \n - ?play (title or link): play a song. \n ?stop: stop playing songs. \n - ?skip: skip the current song. \n - ?pause: pause current song. \n - ?resume: resume current song. \n ?shuffle: shuffle the songs in the queue.",
+          content: "All commands start with '?'. \n \n List of basic commands: \n - ?joke: sends a funny joke \n - ?meme: sends a meme. \n \n List of musicplayer commands: \n - ?play (title or link): play a song. \n - ?stop: stop playing songs. \n - ?skip: skip the current song. \n - ?pause: pause current song. \n - ?resume: resume current song. \n - ?shuffle: shuffle the songs in the queue.",
         });
         break;
 
